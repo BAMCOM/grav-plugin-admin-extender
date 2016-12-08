4e903d99b41ca60b2e25dec12c8b9c89bf800faa
Matthew Dean <matthewdean.me@gmail.com>
2016-12-08 10:59:48 -0800

    Update readme / added theme_custom_url variable

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# grav-plugin-admin-extender
-Allows customization
+# Grav Admin Extender
+
+This Grav plugin allows customization of the Grav admin interface.
+
+1. Create a custom admin theme in your themes folder. Don't add the usual theme PHP, YAML, and Blueprints files, as you don't want the admin theme to show up in your list of installable front-end themes.
+
+2. Install the Admin Extender. Click on the Plugin to change the theme folder name to your custom admin theme. This will add your themes 
+
+3. If you want to completely replace all CSS / JS references (`theme_url` variable in Twig) in plugins/admin/themes/grav to your custom theme, change that setting to Yes.
+
+4. Admin Extender adds a `theme_custom_url` variable to your templates (regardless of the override setting) so you can import extend the assets with CSS / JS of your own in your custom theme folder.
---
+++ admin-extender.php
@@ -0,0 +1,65 @@
+<?php
+namespace Grav\Plugin;
+
+use Grav\Common\Plugin;
+
+/**
+ * Class Plugin
+ * @package Grav\Plugin
+ */
+class AdminExtenderPlugin extends Plugin
+{
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents() {
+      return [
+        'onPluginsInitialized' => ['onPluginsInitialized', 0]
+      ];
+    }
+
+    /**
+     * Initialize the plugin
+     */
+    public function onPluginsInitialized() {
+      if ($this->isAdmin()) {
+        $this->enable([
+          'onTwigTemplatePaths' => ['onTwigTemplatePaths', 0],
+          'onTwigSiteVariables' => ['onTwigSiteVariables', 0]
+        ]);
+      } 
+    }
+
+  public function onTwigTemplatePaths() {
+
+    $theme_name = $this->config->get('plugins.admin-extender.theme', 'custom-admin');
+    $twig_paths = $this->grav['twig']->twig_paths;
+    array_unshift($twig_paths, realpath(__DIR__ . '/../../themes/' . $theme_name . '/templates'));
+
+    $this->grav['twig']->twig_paths = $twig_paths;
+
+    $admin = $this->grav['admin'];
+    $twig = $this->grav['twig']->twig();
+    
+    $this->config->set('plugins.admin.theme', $theme_name);
+    $admin->theme = $theme_name;
+    
+  }
+
+  public function onTwigSiteVariables() {
+
+    $override = $this->config->get('plugins.admin-extender.override_url');
+    
+    
+    $theme_name = $this->config->get('plugins.admin-extender.theme', 'custom-admin');
+    $theme_url = '/' . ltrim($this->grav['locator']->findResource('user://themes/' . $theme_name,
+            false), '/');
+
+    if ($override) {
+      $this->grav['twig']->twig_vars['theme_url'] = $theme_url;
+    }
+    $this->grav['twig']->twig_vars['theme_custom_url'] = $theme_url;
+
+  }
+
+}
